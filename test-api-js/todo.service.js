06b8fbe3d02d434eb2c10a63e20a0c4680d34993
preethiyetcharla <113061317+preethiyetcharla@users.noreply.github.com>
2022-11-07 22:56:51 -0600

    Test cases completed

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -23,16 +23,33 @@ class todoservice{
     }
 
     add_todo(todo){
+        
+        this.todos=this.todos.todo.concat(todo);
+        console.log(this.todos);
         // Your code here
     }
+
 
     delete_todo(id){
+        this.todos = this.todos.filter(todo => todo.description !== id)
+        console.log(this.todos);
+        // Your code here
+    }
+    
+    update_todo(id, todo){
+
+        this.todos.map(upt => {
+            if (upt.description === id) {
+                upt.title = todo;
+                upt.description=todo;
+                upt.done=todo
+            }
+            //return todo
+        })
+        console.log(this.todos);
         // Your code here
     }
 
-    update_todo(id, todo){
-        // Your code here
-    }
 }
 
 
--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -19,6 +19,22 @@ describe('todo test suite', () => {
         expect(todo_service.get_todos().todo.length).toEqual(3);
     });
 
+    test("add_todo", () => {
+        todo_add={
+            "title": "Test_Case_1",
+            "description": "Test_Description_1",
+            "done": false}
+            todo_service.add_todo(todo_add);
+            expect(todo_service.get_todos().length).toEqual(4);});
+
+    test("delete_todo", () => {
+        todo_service.delete_todo("D1");
+        expect(todo_service.get_todos().length).toEqual(1);
+    });
+    test("update_todo", () => {
+        todo_service.update_todo("Test_Description_1","Update_Test-1");
+        expect(todo_service.get_todos().filter(test=>test.description==="Update_Test-1").length).toEqual(1);
+     });
     
 
     // Write all your test cases here that corresponds to software requirements
